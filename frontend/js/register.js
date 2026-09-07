7ceec16f6b86cab8aeae75c388176f187be9e403
Frame <shogunraiden2006@protonmail.com>
2026-09-07 22:55:16 +0700

    Update register.js

--- frontend/js/register.js
+++ frontend/js/register.js
@@ -1,10 +1,5 @@
 // ไม่ต้องระบุ Domain บน Render เพราะเป็น Single Web Service (ใช้ Relative Path ได้เลย)
-const API_URL = (
-    window.location.hostname === 'localhost' || 
-    window.location.hostname === '127.0.0.1'
-)
-    ? 'http://localhost:5000'
-    : '';
+const API_URL = '';
 
 document.addEventListener('DOMContentLoaded', () => {
     const username = document.getElementById("username");
